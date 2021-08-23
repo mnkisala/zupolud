6206f1f85a5f1b16f879f442219ded237f9f34fd
mnkisala <69649738+mnkisala@users.noreply.github.com>
2021-08-23 13:47:55 +0000

    Deploying to gh-pages from @ mnkisala/zupolud@56f566a79725d9054ab33525b097b09031162dc7 🚀

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./components/App"
-
-export default function main() {
-  const view = document.getElementById("react-view")
-
-  ReactDOM.render(<App />, view)
-}
